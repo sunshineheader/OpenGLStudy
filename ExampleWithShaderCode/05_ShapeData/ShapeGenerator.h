16f33c0a403fddefe688f579a58c13bb0892f08c
sunshineheader <sunshineheader@gmail.com>
2015-09-16 11:15:26 +0800

    封装数据 ShapeData MyTriangle = ShapeGenerator::makeTriangle();
    准备向cube出发

---
+++ ExampleWithShaderCode/05_ShapeData/ShapeGenerator.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <glm\glm.hpp>
+#include "Vertex.h"
+#include "ShapeData.h"
+class ShapeGenerator
+{
+public:
+	ShapeGenerator();
+	~ShapeGenerator();
+public:
+	static ShapeData makeTriangle();
+};
+
